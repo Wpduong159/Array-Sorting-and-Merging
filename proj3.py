6309e8c209c9d7def3c0f9a1d292c7fd953fbc9b
William Pham Duong <wpduong@csu.fullerton.edu>
2022-04-22 22:27:38 -0700

    Update proj3.py

--- proj3.py
+++ proj3.py
@@ -102,29 +102,29 @@ if __name__ == '__main__':
 
 	algorithm1()
 
-  array_1 = [[2,5,9,21], [-1,0,2], [-10,81,121], [4,6,12,20,150]]
-  array_2 = [[10,17,18,21,29], [-3,0,3,7,8,11], [81,88,121,131], [9,11,12,19,29]]
-  array_3 = [[-4,-2,0,2,7], [4,6,12,14], [10,15,25], [5,6,10,20,24]]
+	array_1 = [[2,5,9,21], [-1,0,2], [-10,81,121], [4,6,12,20,150]]
+	array_2 = [[10,17,18,21,29], [-3,0,3,7,8,11], [81,88,121,131], [9,11,12,19,29]]
+	array_3 = [[-4,-2,0,2,7], [4,6,12,14], [10,15,25], [5,6,10,20,24]]
 
 
-  print("Pre-merged Example Arrays:")
-  print('{}\n{}\n{}'.format(array_1, array_2,array_3))
-  print("\nMerged Arrays")
+	print("Pre-merged Example Arrays:")
+	print('{}\n{}\n{}'.format(array_1, array_2,array_3))
+	print("\nMerged Arrays")
 
 
-  sorted_1 = merger(array_1[0],array_1[1])
-  sorted_2 = merger(array_1[2], array_1[3])
-  sorted_3 = merger(sorted_1, sorted_2)
+	sorted_1 = merger(array_1[0],array_1[1])
+	sorted_2 = merger(array_1[2], array_1[3])
+	sorted_3 = merger(sorted_1, sorted_2)
 
-  print(sorted_3)
+	print(sorted_3)
 
-  sorted_1 = merger(array_2[0],array_2[1])
-  sorted_2 = merger(array_2[2], array_2[3])
-  sorted_3 = merger(sorted_1, sorted_2)
+	sorted_1 = merger(array_2[0],array_2[1])
+	sorted_2 = merger(array_2[2], array_2[3])
+	sorted_3 = merger(sorted_1, sorted_2)
 
-  print(sorted_3)
-  sorted_1 = merger(array_3[0],array_3[1])
-  sorted_2 = merger(array_3[2], array_3[3])
-  sorted_3 = merger(sorted_1, sorted_2)
+	print(sorted_3)
+	sorted_1 = merger(array_3[0],array_3[1])
+	sorted_2 = merger(array_3[2], array_3[3])
+	sorted_3 = merger(sorted_1, sorted_2)
 
-  print(sorted_3)
+	print(sorted_3)
